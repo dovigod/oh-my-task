e58ff24008a605caad3d47d1278d120a5d7d2846
Ji Sang Seo <inaki96@naver.com>
2024-02-08 14:08:46 +0900

    feature: history done

--- cli/oh-my-cli-init.mjs
+++ cli/oh-my-cli-init.mjs
@@ -9,10 +9,24 @@ async function main() {
     .description(`Initialize settings for current project.\n
     ${chalk.green("(Entry Command for every project)")}`);
 
-  program.option("-g, --global", "Use global config");
+  program.option("-g, --global", `Setup History file on global location.`);
   program.parse();
 
-  await context.config.createConfig();
+  const options = program.opts();
+
+  await exec(options);
 }
 
 await main().catch((err) => console.error(err));
+
+async function exec(
+  options = {
+    global: false,
+  }
+) {
+  if (options.global) {
+    context.manifest.initializeHistory();
+    return;
+  }
+  await context.manifest.createConfig();
+}
