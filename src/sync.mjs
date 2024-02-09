c11e87b070ebc463cd3b19028dcf15633db570c5
Ji Sang Seo <inaki96@naver.com>
2024-02-10 00:19:19 +0900

    feature: sync command

--- src/sync.mjs
+++ src/sync.mjs
@@ -3,6 +3,10 @@ import * as context from "./context.mjs";
 import * as input from "./input.mjs";
 import * as markdown from "./markdown.mjs";
 import { Formatter } from "./formatter.mjs";
+import fs from "fs/promises";
+
+const PLACEHOLDER_START = "<!-->Start of Placeholder of OH-MY-TASK<-->";
+const PLACEHOLDER_END = "<!-->End of Placeholder of OH-MY-TASK<-->";
 
 export async function sync() {
   const config = await context.manifest.getConfig();
@@ -27,15 +31,38 @@ export async function sync() {
     };
   });
 
-  const syncToFileType = getFileType(syncTo);
+  const syncToFileType = getSyncFileType(syncTo);
 
   const formatter = new Formatter(syncToFileType, heading, taskList);
   const formattedStringToInject = formatter.run();
 
-  console.log(formattedStringToInject);
+  await inject(syncTo, formattedStringToInject);
+  console.log(chalk.green(`Tasks Successfully synchronized to ${syncTo}`));
 }
 
-function getFileType(file) {
+/**
+ * injects `content` inside `PLACEHOLDER_START` & PLACEHOLDER_END`
+ *
+ * @param {string} filepath - file path where to sync tasks
+ * @param {string} content - string content which is obtained by `Formatter.run()`
+ */
+async function inject(filepath, content) {
+  const fileContent = await fs.readFile(filepath, { encoding: "utf-8" });
+
+  // discard file content inside placeholder  (if previous content exists)
+  let updatedContent = fileContent.replace(
+    `\/${PLACEHOLDER_START}.*${PLACEHOLDER_END}/`,
+    ""
+  );
+  // inject
+  updatedContent = updatedContent.replace(
+    PLACEHOLDER_START,
+    `${PLACEHOLDER_START}\n${content}`
+  );
+  await fs.writeFile(filepath, updatedContent).catch(console.error);
+}
+
+function getSyncFileType(file) {
   if (markdown.isMarkdown(file)) {
     return "md";
   } else {
