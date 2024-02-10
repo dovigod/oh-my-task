2f4a8fd8774593cc8c35409e245c18d168fc51ef
Ji Sang Seo <inaki96@naver.com>
2024-02-11 01:01:12 +0900

    [Feature] basic git commands (#11)
    * update: create & checkout
    
    * update: basic git command impl
    
    * update: setting upstream func + push function now can set upstream
    
    * update: push function gets current branch to set upstream
    
    * update: func :: selectBranch

--- src/context.mjs
+++ src/context.mjs
@@ -2,6 +2,11 @@ import { Manifest } from "./manifest.mjs";
 import os from "os";
 
 const DefaultHistoryPath = `${os.homedir()}/.omt/history.yaml`;
+// const userConfigPath = `${os.homedir()}/.omt/userConfig.yaml`;
 const configPath = `${process.cwd()}/.omt.yaml`;
 
-export const manifest = new Manifest(configPath, DefaultHistoryPath);
+export const manifest = new Manifest(
+  configPath,
+  DefaultHistoryPath
+  // userConfigPath
+);
---
+++ src/git.mjs
@@ -0,0 +1,121 @@
+import * as input from "./input.mjs";
+import { execSync, spawnSync } from "child_process";
+
+function git(
+  command,
+  params,
+  options = {
+    encoding: "utf-8",
+    stdio: "inherit",
+  }
+) {
+  if (params instanceof Array) {
+    return spawnSync("git", [command, ...params], options);
+  } else {
+    return spawnSync("git", [command, params], options);
+  }
+}
+
+export async function create(branch, baseBranch = "origin/develop") {
+  const createResult = git("branch", [branch, baseBranch], {
+    stdio: ["inherit", process.stdout, process.stderr],
+  });
+
+  if (createResult.status !== 0) {
+    throw new Error("Failed to create branch");
+  }
+
+  return branch;
+}
+
+export async function checkout(branch) {
+  const checkoutResult = git("checkout", branch);
+
+  if (checkoutResult.status !== 0) {
+    throw new Error("Failed to checkout");
+  }
+  return branch;
+}
+
+export async function push(withSettingCurrentBranchUpStream = false) {
+  let pushResult;
+
+  if (withSettingCurrentBranchUpStream) {
+    const currentBranch = getCurrentBranchName();
+
+    pushResult = git("push", ["-u", "origin", currentBranch], {
+      encoding: "utf-8",
+      stdio: "inherit",
+    });
+  } else {
+    pushResult = git("push");
+  }
+
+  if (pushResult.status !== 0) {
+    throw new Error("Failed to push");
+  }
+
+  return true;
+}
+
+export async function pull() {
+  const pullResult = git("pull");
+  if (pullResult.status !== 0) {
+    throw new Error("Failed to pull");
+  }
+
+  return true;
+}
+
+export async function fetch() {
+  const result = git("fetch");
+
+  if (result.status !== 0) {
+    throw new Error("Failed to fetch prune");
+  }
+}
+
+export async function merge(branch) {
+  const mergeResult = git("merge", branch);
+
+  if (result.status !== 0) {
+    throw new Error("Failed to merge");
+  }
+}
+
+/**
+ *
+ * @param {string} remoteBranch - origin/${branchName}
+ */
+export async function setUpStream(remoteBranch) {
+  const setUpStreamResult = git("branch", ["--set-upstream-to", remoteBranch]);
+
+  if (setUpStreamResult.status !== 0) {
+    throw new Error("Failed to set upstream");
+  }
+}
+
+export function getCurrentBranchName() {
+  const output = execSync(`git branch --show-current`, { encoding: "utf8" });
+  return output.trim();
+}
+
+export async function selectBranch(message = "Select Branch", remote = true) {
+  const command = remote
+    ? "git branch -a --format='%(refname:short)'"
+    : "git branch --format='%(refname:short)'";
+
+  const branchRetrieveResult = execSync(command, {
+    encoding: "utf8",
+  });
+
+  const branches = branchRetrieveResult
+    .split("\n")
+    .filter((branch) => branch !== "");
+
+  return input.select(
+    message,
+    branches.map((branch) => ({ value: branch })),
+    ["value"]
+  );
+}
