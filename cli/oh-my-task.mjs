af7810d93afbad5067c497f2085ea11f83eb9f19
Ji Sang Seo <inaki96@naver.com>
2024-02-12 14:29:34 +0900

    chore: decoration added (#21)

--- cli/oh-my-task.mjs
+++ cli/oh-my-task.mjs
@@ -2,34 +2,66 @@
 
 import { Command } from "commander";
 import { readFileSync } from "fs";
+import chalk from "chalk";
 
 const PACKAGE = JSON.parse(
   readFileSync(new URL("../package.json", import.meta.url), "utf-8")
 );
 
-const program = new Command();
+async function runner() {
+  const program = new Command();
 
-program
-  .name(PACKAGE.name)
-  .description(PACKAGE.description)
-  .version(PACKAGE.version);
+  console.log(`
+  ${chalk.redBright(` ██████  ██   ██`)}     ${chalk.greenBright(
+    `███    ███ ██    ██`
+  )}     ${chalk.blueBright(`████████  █████  ███████ ██   ██`)}   ${chalk.cyan(
+    `██`
+  )}\n
+  ${chalk.redBright(`██    ██ ██   ██`)}     ${chalk.greenBright(
+    `████  ████  ██  ██`
+  )}      ${chalk.blueBright(
+    `   ██    ██   ██ ██      ██  ██`
+  )}    ${chalk.cyan(`██`)}\n
+  ${chalk.redBright(`██    ██ ███████`)}     ${chalk.greenBright(
+    `██ ████ ██   ████`
+  )}       ${chalk.blueBright(
+    `   ██    ███████ ███████ █████`
+  )}     ${chalk.cyan(`██`)}\n
+  ${chalk.redBright(`██    ██ ██   ██`)}     ${chalk.greenBright(
+    `██  ██  ██    ██`
+  )}        ${chalk.blueBright(
+    `   ██    ██   ██      ██ ██  ██`
+  )}    ${chalk.cyan(``)}\n
+  ${chalk.redBright(` ██████  ██   ██`)}     ${chalk.greenBright(
+    `██      ██    ██`
+  )}        ${chalk.blueBright(
+    `   ██    ██   ██ ███████ ██   ██`
+  )}   ${chalk.cyan(`██`)}\n`);
 
-// commands
+  program
+    .name(PACKAGE.name)
+    .description(PACKAGE.description)
+    .version(PACKAGE.version);
 
-program.command("init", "Initialize settings");
-program.command("create", "Create Task To Do");
-program.command("list", "List up todos");
-program.command("sync", "Synchronize tasks from history.yaml to syncTo file");
-program.command("select", "Select task to work");
-program.command(
-  "pr",
-  "Request Pull Request from current branch to Base branch"
-);
-program.command(
-  "finish",
-  "Finish Task and checkout to base branch. This command is use to end Task life cycle"
-);
+  // commands
 
-// parsings
-program.showHelpAfterError();
-program.parse();
+  program.command("init", "Initialize settings");
+  program.command("create", "Create Task To Do");
+  program.command("list", "List up todos");
+  program.command("sync", "Synchronize tasks from history.yaml to syncTo file");
+  program.command("select", "Select task to work");
+  program.command(
+    "pr",
+    "Request Pull Request from current branch to Base branch"
+  );
+  program.command(
+    "finish",
+    "Finish Task and checkout to base branch. This command is use to end Task life cycle"
+  );
+
+  // parsings
+  program.showHelpAfterError(true);
+  program.parse();
+}
+
+await runner().catch((e) => {});
