ce620cbc7128e9aa184c95ceed26fa8ff74d25b2
Ji Sang Seo <inaki96@naver.com>
2024-02-17 17:31:56 +0900

    improvement: sync optional when pr

--- cli/oh-my-task-pr.mjs
+++ cli/oh-my-task-pr.mjs
@@ -7,13 +7,27 @@ async function pullRequestCommand() {
   const program = new Command();
   program
     .name("oh-my-task-pr")
+    .option(
+      "-s , --sync",
+      "Sync task list and inject changes in previous commit."
+    )
     .description(
       `Request Pull Request from currently working Task (current branch) to Base branch`
     );
 
   program.parse();
 
-  await pullRequest();
+  const options = program.opts();
+
+  const prOptions = {
+    sync: false,
+  };
+
+  if (options.sync) {
+    prOptions.sync = true;
+  }
+
+  await pullRequest(prOptions);
 }
 
 await pullRequestCommand().catch((error) => console.error(error));
